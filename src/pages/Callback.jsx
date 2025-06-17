7b26a237304649bfae9306ecf4e934ab56445f36
Warren Troublefield <swegmoneylife@gmail.com>
2025-06-17 19:59:56 -0400

    Update Callback.jsx

--- src/pages/Callback.jsx
+++ src/pages/Callback.jsx
@@ -1,23 +1,21 @@
-// src/Callback.jsx
-import { useEffect } from 'react';
+// src/pages/callback.jsx
+import React, { useEffect } from "react";
 
 function Callback() {
   useEffect(() => {
     const hash = window.location.hash.substring(1);
     const params = new URLSearchParams(hash);
-    const accessToken = params.get('access_token');
+    const accessToken = params.get("access_token");
 
     if (accessToken) {
-      localStorage.setItem('spotify_access_token', accessToken);
-      window.location.replace('/');
-    } else {
-      console.error('Spotify token missing.');
+      localStorage.setItem("spotify_access_token", accessToken);
+      window.location.href = "/";
     }
   }, []);
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Redirecting...</h1>
+    <div className="text-center mt-10 text-lg">
+      Redirecting...
     </div>
   );
 }
