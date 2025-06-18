073e3c9b9e6dfbe9eb8e1009fb460975b1b61811
Warren Troublefield <swegmoneylife@gmail.com>
2025-06-18 15:32:02 -0400

    Create Gallery.jsx

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GalleryCards from "./GalleryCards";
+
+const Gallery = () => {
+  return (
+    <section className="py-10">
+      <h2 className="text-2xl font-semibold mb-4">Gallery</h2>
+      <GalleryCards />
+    </section>
+  );
+};
+
+export default Gallery;
