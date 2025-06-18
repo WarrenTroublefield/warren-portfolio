bdb507419a6861ea9f2a49476a7cb61e1cde5e65
Warren Troublefield <swegmoneylife@gmail.com>
2025-06-18 15:57:36 -0400

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,8 +3,13 @@ import GalleryCards from "./GalleryCards";
 
 const Gallery = () => {
   return (
-    <section className="py-10">
-      <h2 className="text-2xl font-semibold mb-4">Gallery</h2>
+    <section className="mb-16" id="gallery">
+      <h2 className="text-xl font-semibold text-zinc-900 dark:text-white mb-4">
+        Gallery
+      </h2>
+      <p className="text-zinc-700 dark:text-zinc-300 mb-4">
+        Here are a few snapshots from things I’ve worked on or certifications I’ve earned.
+      </p>
       <GalleryCards />
     </section>
   );
